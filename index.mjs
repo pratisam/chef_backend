530b0c4f0bfa0dea6e07924b5432d8417777c65d
Kyubs1610 <diego.decort@gmail.com>
2023-02-02 13:35:40 +0100

    script commit

--- index.mjs
+++ index.mjs
@@ -17,7 +17,7 @@ const app = express();
 const PORT = process.env.PORT;
 app.use(bodyParser.json());
 app.use(cookie());
-app.use(cors({origin: 'http://localhost:3000'}));
+app.use(cors({origin: 'http://localhost:3000', credentials: true}));
 
 
 //Cuisine Types
