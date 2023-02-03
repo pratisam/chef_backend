41ff40351d06171b00742104c070b4e12c075bd4
andriivam <andriivlasiuk2@gmail.com>
2023-02-03 15:40:14 +0100

    create set up for herocu

--- database/client.mjs
+++ database/client.mjs
@@ -5,21 +5,35 @@ dotenv.config()
 const { Client } = pkg;
 
 
-const client = new Client({
-connectionString: process.env.DATABASE_URL,
-ssl: {
-    rejectUnauthorized: false
-}
-});
+// const client = new Client({
+// connectionString: process.env.DATABASE_URL || "development",
+// ssl: {
+//     rejectUnauthorized: false
+// }
 
-client.connect((err) => {
-if (err) {
-    console.error('connection error', err.stack)
-} else {
-    console.log('connected')
-}
-})
+// });
 
+// client.connect((err) => {
+// if (err) {
+//     console.error('connection error', err.stack)
+// } else {
+//     console.log('connected')
+// }
+// })
 
+const client = (() => {
+    if (process.env.NODE_ENV !== 'production') {
+        return new Client({
+            connectionString: process.env.DATABASE_URL,
+            ssl: false
+        });
+    } else {
+        return new Client({
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+                rejectUnauthorized: false
+              }
+        });
+    } })();
 
 export default client;
