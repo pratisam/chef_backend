f009a707603e5a5145580379b4954245ff684af4
Pranavi <pranavi@Pranavis-MacBook-Pro.local>
2023-02-05 21:38:54 +0100

    add

--- index.mjs
+++ index.mjs
@@ -21,7 +21,7 @@ const app = express();
 const PORT = process.env.PORT;
 app.use(bodyParser.json());
 app.use(cookie());
-app.use(cors({origin: 'https://homechefbackend.herokuapp.com'}));
+app.use(cors({origin: 'http://localhost:3000'}));
 
 console.log('hello world')
 //Cuisine Types
